9013200a24532899b43a6ae1ecc3645062d36fad
Bastian Schnieders <bastian.schnieders@stepstone.be>
2018-04-13 14:48:12 +0200

    squash! work in progress

--- src/AbstractJsonSerializableCollection.php
+++ src/AbstractJsonSerializableCollection.php
@@ -13,7 +13,7 @@ abstract class AbstractJsonSerializableCollection extends AbstractPaginatedColle
      * @param int $limit
      * @param int $totalAmount
      */
-    public function __construct(array $items, int $offset, int $limit, int $totalAmount)
+    public function __construct(AbstractCollection $items, int $offset, int $limit, int $totalAmount)
     {
         parent::__construct($items, $offset, $limit, $totalAmount);
     }
@@ -42,4 +42,18 @@ abstract class AbstractJsonSerializableCollection extends AbstractPaginatedColle
 
 
     abstract protected static function serializeItem($item): array;
+
+    function __call($methodName, $arguments)
+    {
+        if ($this->isSaveToDelegate($methodName)) {
+            return call_user_func_array([$this->original, $methodName], $arguments);
+        } else {
+            throw new DelegationException($this, $this->original, $methodName);
+        }
+    }
+
+    private function isSaveToDelegate($methodName): bool
+    {
+        return method_exists($this->original, $methodName) && !in_array($methodName, static::FORBIDDEN_METHOD_NAMES);
+    }
 }
